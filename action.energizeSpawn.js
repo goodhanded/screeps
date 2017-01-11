efdc30c9d9fe1c779bebe789b2dfc5fa62cf92ef
Keith Morris <keith.a.morris@gmail.com>
2017-01-10 19:39:34 -0500

    Fixed a bug caused by creeps wandering into the next room.

--- action.energizeSpawn.js
+++ action.energizeSpawn.js
@@ -10,7 +10,7 @@ module.exports = {
         
         var spawn = nearest.spawn(creep)
         
-        if (spawn.energy == spawn.energyCapacity) {
+        if (!spawn || spawn.energy == spawn.energyCapacity) {
             return false;
         }
         
